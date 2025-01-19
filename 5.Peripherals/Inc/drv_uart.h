e55768b5eb6c6e006725a8e22105a53687b11956
12249 <12755011+z12249@user.noreply.gitee.com>
2025-01-19 17:25:54 +0800

    04_RS232

--- 5.Peripherals/Inc/drv_uart.h
+++ 5.Peripherals/Inc/drv_uart.h
@@ -1,6 +1,10 @@
 #ifndef	__DRV_UART_H
 #define __DRV_UART_H
 
+// RS232's serial
+int Drv_USART1_Init(void);
+
+// RS485
 int Drv_USART3_Init(void);
 int Drv_USART3_Write(const unsigned char *pData, const unsigned short Length);
 int Drv_USART3_Read(unsigned char *pData, const unsigned int Length);
